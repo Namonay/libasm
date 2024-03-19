f9396fc10ee5910e3c678040ab4f815ba278eee0
Namonay <thek7995@gmail.com>
2024-03-19 12:45:17 +0100

    Final commit :)

--- ft_write.s
+++ ft_write.s
@@ -2,26 +2,10 @@ global ft_write
 extern __errno_location
 
 ft_write:
-	cmp rdi, 0
-	jl .err_fd
-	test rsi, rsi
-	je .err_buffer
 	mov eax, 0x1 ; put the syscall number of write
 	syscall
 	test rax, rax
 	js .error
-	ret
-
-.err_fd:
-	call __errno_location wrt ..plt
-	mov byte [rax], 22
-	mov rax, -1
-	ret
-
-.err_buffer:
-	call __errno_location wrt ..plt
-	mov byte [rax], 22
-	mov rax, -1
 	ret
 
 .error:
